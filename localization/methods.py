8505770e18f00d824156764e29a9934f7dc42598
Tony Hirst <tony.hirst@gmail.com>
2016-08-30 18:58:04 +0100

    Update methods.py

--- localization/methods.py
+++ localization/methods.py
@@ -1,7 +1,7 @@
 import numpy as num
-import geometry as gx
+from . import geometry as gx
 from scipy.optimize import minimize,fmin_cobyla
-from .find_centroid import maxPol
+from . import find_centroid.maxPol as maxPol
 
 class cornerCases(Exception):
     def __init__(self, value):
